b8f57f8d0c533d293f1ac5c9d6735d06b6d0459b
celiance <celia@celiance.com>
2020-10-28 14:08:30 +0100

    UPDATE Grundgerüst

--- footer.php
+++ footer.php
@@ -8,16 +8,16 @@
         <a href="#"><img src="" alt="">menü</a>
       </li>
       <li>
-        <a href="#"><img src="" alt="">home</a>
+        <a href="/MeinInventar.php"><img src="" alt="">home</a>
       </li>
       <li>
-        <a href="#"><img src="" alt="">artikel erfassen</a>
+        <a href="/artikelErfassen.php"><img src="" alt="">artikel erfassen</a>
       </li>
       <li>
-        <a href="#"><img src="" alt="">push</a>
+        <a href="/alert.php"><img src="" alt="">push</a>
       </li>
       <li>
-        <a href="#"><img src="" alt="">profil</a>
+        <a href="/profil.php"><img src="" alt="">profil</a>
       </li>
     </ul>
   </nav>
--- produktseite.php
+++ produktseite.php
@@ -5,13 +5,15 @@
 
     <!--Produktanzeige-->
     <h2>Produkt Nr. 1</h2>
-      <div class="">
+    <div class="">
       <img src="assets/testbild.jpg" alt="bild des Produktes">
       <p>Beschreibugn vom Produkt hier kommt bizli Text und so.</p>
       <h5>Gekauft am</h5>
       <h5>17/05/20</h5>
-      </div>
+    </div>
 
+    <button type="button" name="button" onclick="window.location.href='/produktVerkaufen.php'">Produkt verkaufen</button>
+    <h5 class="delete">Produkt löschen</h5>
 
   </main>
 <?php include 'footer.php';?>
