601c86175a1fefc7987dbd47cc7db5e05d53bcd5
Saskia Marty <martysaskia1@hotmail.com>
2020-12-16 14:45:53 +0100

    index update

--- index.php
+++ index.php
@@ -13,14 +13,15 @@
 <body>
     <section class="onepage">
         <main>
-             <h2>Nutzte minimize und behalte den Überblick über dein Zuhause.</h2>
-              <p> Der durchschnittliche Haushalt besteht aus rund 10’000 Gegenständen, benutzt werden im Schnitt
-                  allerdings nur 500. Das heisst 9’500 Gegenstände brauchen Platz, Geld und Zeit. Minimize hilft dir unnötigen Kram loszuwerden und mehr Freiheit zu erhalten. Unser Credo: LESS THINGS, MORE WINGS</p>
-            <button type="button" name="button" onclick="window.location.href='/rundgang_1.php'">
-              <i class="fas fa-arrow-circle-right fa-3x"><p>Rundgang starten</p></i>
-            </button>
-            <button type="button" name="button" onclick="window.location.href='/login.php'">
-              <p>Login</p>
+            <h2> Rundgang starten. </h2>
+             <p>Nutzte minimize und behalte den Überblick über dein Zuhause.</p>
+
+             <button type="button" name="button" onclick="window.location.href='/rundgang_1.php'">
+                 <i class="fas fa-arrow-circle-right fa-3x"></i>
+              </button>
+
+            <button class="loginbutton" type="button" name="button" onclick="window.location.href='/login.php'">
+              <h6>Login</h6>
             </button>
         </main>
 
