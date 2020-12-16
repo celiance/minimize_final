3c3e3714063f22a6b30b058e1ba7c0c6f3f0326a
celiance <celia@celiance.com>
2020-12-16 11:13:21 +0100

    update index login rundgang starten

--- index.php
+++ index.php
@@ -6,7 +6,11 @@
               <p> Der durchschnittliche Haushalt besteht aus rund 10’000 Gegenständen, benutzt werden im Schnitt
 allerdings nur 500. Das heisst 9’500 Gegenstände brauchen Platz, Geld und Zeit. Minimize hilft dir unnötigen Kram loszuwerden und mehr Freiheit zu erhalten. Unser Credo: LESS THINGS, MORE WINGS</p>
             <button type="button" name="button" onclick="window.location.href='/rundgang_1.php'">
-              <i class="fas fa-arrow-circle-right fa-3x"></i>
+              <i class="fas fa-arrow-circle-right fa-3x"><p>Rundgang starten</p></i>
+            </button>
+            <button type="button" name="button" onclick="window.location.href='/login.php'">
+              <p>Login</p>
+            </button>
         </main>
 
 <?php include 'footer.php';?>
