882778b37693a38446663d11aff73c1c1691c058
celiance <celia@celiance.com>
2020-12-16 13:41:19 +0100

    update Produkte werden gepusht, wenn sie älter als 6 Monate alt sind

--- produktseite.php
+++ produktseite.php
@@ -1,19 +1,22 @@
 <?php
-  $unterscheidung = true;
-?>
 
-<?php include 'header.php';?>
+  include ('header.php');
+  $unterscheidung = true;
+  $all_products = get_product($user_id);
+
+
+?>
   <!-- MAIN MAIN -->
   <body class="produktseite">
       <!--Produktanzeige-->
       <div class="produktonly">
         <main>
           <div class="inhaltproduktonly">
-              <h3> Stabmixer Super Turbo </h3>
+              <h3>Stabmixer Super Turbo </h3>
               <img class="testbild" src="assets/testbild.jpg" alt="testbild" width="100">
-              <p> hier kommt eine Bescheibung zum Produkt</p>
-              <p> Gekauft am </p>
-              <p> 5.12.12</p>
+              <p>hier kommt eine Bescheibung zum Produkt</p>
+              <p>Gekauft am </p>
+              <p>5.12.12</p>
               <a href="">
                 <button class="produktonlyedit">
                   <i class="fas fa-pen"></i>
