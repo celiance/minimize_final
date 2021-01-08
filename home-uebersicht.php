34af68817ca3a5495064beac1ce2ae94dfa61a2e
celiance <>
2021-01-08 11:15:02 +0100

    update home uebersicht

--- home-uebersicht.php
+++ home-uebersicht.php
@@ -4,31 +4,30 @@
   include 'login-wall.php';
 
 ?>
-  <body class="home">
-    <section class="home">
-        <main>
-            <h2>Hallo <br>
-              <?php echo $user['name']; ?></h2>
-              <div class="box">
-                <p>Mein Inventar ansehen</p>
+<body class="home">
+  <section class="home">
+      <main>
+        <h2>Hallo <br>
+          <?php echo $user['name']; ?>
+        </h2>
+          <div class="box">
+            <p>Mein Inventar ansehen</p>
+            <a href="/MeinInventar.php">
+            <button>
+              <i class="fas fa-arrow-right "></i>
+            </button>
+            </a>
+          </div>
 
-                <a href="/MeinInventar.php">
-                  <button>
-                    <i class="fas fa-arrow-right "></i>
-                  </button>
-                </a>
-              </div>
+            <div class="box">
+              <p>Brauchst du das noch?</p>
 
-              <div class="box">
-                <p>Brauchst du das noch?</p>
-
-                <a href="/alert.php">
-                  <button>
-                    <i class="fas fa-arrow-right "></i>
-                  </button>
-                </a>
-              </div>
-
-
-        </main>
+              <a href="/alert.php">
+                <button>
+                  <i class="fas fa-arrow-right "></i>
+                </button>
+              </a>
+            </div>
+      </main>
+    </section>
       <?php include "footer.php";?>
